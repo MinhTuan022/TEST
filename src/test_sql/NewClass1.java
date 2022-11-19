39ef75a69416d86c27a93b9f086644b5e9c04296
ADMIN <ADMIN@DESKTOP-STESN97>
2022-11-19 17:53:23 +0700

    test

---
+++ src/test_sql/NewClass1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test_sql;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class NewClass1 {
+    
+}
